db42352c19bb58211000c00d3a13b43731f9f470
havrylchyk <anastasiia.havrylchyk@oa.edu.ua>
2023-05-04 18:50:13 +0300

    Lab 5, Second commit/done

--- app/Http/Requests/BlogCategoryCreateRequest.php
+++ app/Http/Requests/BlogCategoryCreateRequest.php
@@ -13,7 +13,7 @@ class BlogCategoryCreateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,6 +25,10 @@ class BlogCategoryCreateRequest extends FormRequest
     {
         return [
             //
+            'title' => 'required|min:5|max:200',
+            'slug' => 'max:200',
+            'description' => 'string|max:500|min:3',
+            'parent_id' => 'required|integer|exists:blog_categories,id',
         ];
     }
 }

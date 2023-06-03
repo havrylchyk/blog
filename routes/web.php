dc45663ebda3bbab66b51f401328c8d4f4d8856a
Anastasiia Havrylchyk <anastasiia.havrylchyk@oa.edu.ua>
2023-06-03 23:14:51 +0300

    Lab 11, done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,13 @@ Route::get('/', function () {
 Route::resource('rest', RestTestController::class)->names('restTest');
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
+    Route::group(['prefix' => 'digging_deeper'], function () {
+
+        Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+            ->name('digging_deeper.collections');
+
+    });
 });
 //Адмінка
 $groupData = [
@@ -37,4 +45,9 @@ Route::group($groupData, function () {
     Route::resource('posts', PostController::class)
         ->except(['show'])
         ->names('blog.admin.posts');
+});
+Route::get('/collections', [DiggingDeeperController::class, 'collections']);
+Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+        ->name('digging_deeper.collections');
 });
